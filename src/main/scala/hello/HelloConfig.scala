18e723ef45480303945c01f21472d481582c1733
Mallika Kulkarni <mallikak2709@gmail.com>
2014-09-09 23:39:06 -0700

    Converted the Spring Boot Hello World java application to Scala.

--- src/main/scala/hello/HelloConfig.scala
+++ src/main/scala/hello/HelloConfig.scala
@@ -3,6 +3,9 @@ package hello
 import org.springframework.context.annotation.Configuration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.stereotype.Controller
 
 /**
  * This config class will trigger Spring @annotation scanning and auto configure Spring context.
@@ -13,6 +16,11 @@ import org.springframework.context.annotation.ComponentScan
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@Controller
 class HelloConfig {
-
+  @RequestMapping(value = Array("/"))
+  @ResponseBody
+  def functionName(): String = {
+    return "Hello World!";
+  }
 }
